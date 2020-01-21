57a5a34bd78738a5e3b41a5306cf6dabbe525191
uppsss8888 <80979825696q@gmail.com>
2020-01-21 17:57:56 +0200

    y

--- src/collect/Zapusk.java
+++ src/collect/Zapusk.java
@@ -4,57 +4,104 @@ import abcd.A;
 
 import java.io.InputStream;
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Zapusk {
     public static void main(String[] args) {
+        Map yy = fff(1000, 6);
 
 
-      List <Integer> yy=new Arrayrr<>();
+    }
+    static Map<String, Rez> fff(int slov, int bukvMax) {
+        int razm=(int) (slov * 1.2);
+        Set <String>stt = new HashSet<>(razm);
+        Random rnd = new Random();
 
-        yy.add(6);yy.add(6);yy.add(6);yy.add(6);yy.add(6);yy.add(6);yy.add(6);yy.add(6);
-        List <Integer> yy1=new Arrayrr<>();
-
-        yy1.add(6);yy1.add(6);
-        System.out.println(yy+"do");
-        yy.removeAll(yy1);
-
-
-        System.out.println(yy1);
-    }}
-      class Box{
-  int x;
-
-        @Override
-        public boolean equals(Object o) {
-          if (this == o) return true;
-          if (o == null || getClass() != o.getClass()) return false;
-
-          Box box = (Box) o;
-
-          return x == box.x;
+        for (int i = 0; i < slov; i++) {
+            int bukva=rnd.nextInt(bukvMax);
+            if (bukva<4) bukva=(bukva+4)*2;
+            char [] ch=new char[bukva];
+            for (int j = 0; j <bukva ; j++) {
+                ch[j]=(char) rnd.nextInt(20);
+            }
+            stt.add(new String(ch));
         }
+        Map <String,Rez> mp=new HashMap<>(razm);
+      for (String slovo:stt){
 
-        @Override
-        public int hashCode() {
-          return x;
-        }
+          Set <String> spisRezult=new HashSet<>();
+          int s4et4ik=0;
 
-        public Box(int x) {
+          for (String slovosr:stt){
 
-          this.x = x;
-        }
+              if ( anagr(slovo)  .equals(anagr(slovosr))) {spisRezult.add(slovosr);s4et4ik++;}
+          }
+          Rez temp=new Rez(spisRezult,s4et4ik);
+          mp.put(slovo,temp);
+          System.out.print(slov+"слово  ");
+          temp.spisok.stream().forEach((x)-> System.out.print(x));
+          System.out.print("   количество"+temp.kolichestvo);
+          System.out.println();
 
-        @Override
-        public String toString() {
-          return "Box{" +
-                  "x=" + x +
-                  '}';
-        }
       }
+        return mp;
+    }
+    static String anagr(String str){
+        char [] ch=str.toCharArray();
+       Arrays.sort(ch);
+        return new String(ch);
+}
+
+}
+
+class Box {
+    int x;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Box box = (Box) o;
+
+        return x == box.x;
+    }
+
+    @Override
+    public int hashCode() {
+        return x;
+    }
+
+    public Box(int x) {
+
+        this.x = x;
+    }
+
+    @Override
+    public String toString() {
+        return "Box{" +
+                "x=" + x +
+                '}';
+    }
+}
+
+class Rez <T> {
+    Set<T> spisok;
+    int kolichestvo;
+
+    public Rez(Set<T> spisok, int kolichestvo) {
+        this.spisok = spisok;
+        this.kolichestvo = kolichestvo;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Rez.class.getSimpleName() + "[", "]")
+                .add("spisok=" + spisok)
+                .add("kolichestvo=" + kolichestvo)
+                .toString();
+    }
+}
 
 
 
